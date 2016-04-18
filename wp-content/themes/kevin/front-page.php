5f00e9878683e2a34c93cb04b1a3ed7d0905496f
kcroake <kcroake88@gmail.com>
2016-04-17 22:03:09 -0400

    EON Fail - No Launch This Weekend

--- wp-content/themes/kevin/front-page.php
+++ wp-content/themes/kevin/front-page.php
@@ -2,4 +2,8 @@
 $context = Timber::get_context();
 $post = new TimberPost();
 $context['post'] = $post;
+$args = 'post_type=post&numberposts=-1';
+$works = 'post_type=my-work&numberposts=-1';
+$context['posts'] = Timber::get_posts($args);
+$context['works'] = Timber::get_posts($works);
 Timber::render( array( 'front-page.twig'), $context );
