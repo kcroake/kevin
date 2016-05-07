d83e09c3d2ec8519d9633a1387e6119588b2f16d
kcroake <kcroake88@gmail.com>
2016-05-06 22:33:45 -0400

    Commit fails?

--- wp-content/themes/kevin/functions.php
+++ wp-content/themes/kevin/functions.php
@@ -1,5 +1,86 @@
 <?php
 
+remove_action( 'wp_head', 'feed_links_extra', 3 ); // Display the links to the extra feeds such as category feeds
+remove_action( 'wp_head', 'feed_links', 2 ); // Display the links to the general feeds: Post and Comment Feed
+remove_action( 'wp_head', 'rsd_link' ); // Display the link to the Really Simple Discovery service endpoint, EditURI link
+remove_action( 'wp_head', 'wlwmanifest_link' ); // Display the link to the Windows Live Writer manifest file.
+remove_action( 'wp_head', 'index_rel_link' ); // index link
+remove_action( 'wp_head', 'parent_post_rel_link', 10, 0 ); // prev link
+remove_action( 'wp_head', 'start_post_rel_link', 10, 0 ); // start link
+remove_action( 'wp_head', 'adjacent_posts_rel_link', 10, 0 ); // Display relational links for the posts adjacent to the current post.
+remove_action( 'wp_head', 'wp_generator' ); // Display the XHTML generator that is generated on the wp_head hook, WP version
+
+/* Prevent Login Errors for Security */
+add_filter('login_errors',create_function('$a', "return null;"));
+/* Globally Hide Admin Meta Boxes */
+function hide_meta_boxes() {
+     remove_meta_box('postcustom','post','normal'); // custom fields post
+     remove_meta_box('postcustom','page','normal'); // custom fields page
+
+     //remove_meta_box('commentstatusdiv','post','normal'); // discussion post
+     remove_meta_box('commentstatusdiv','page','normal'); // discussion page
+
+     //remove_meta_box('commentsdiv','post','normal'); // comments post
+     //remove_meta_box('commentsdiv','page','normal'); // comments page
+
+     //remove_meta_box('authordiv','post','normal'); // author post
+     remove_meta_box('authordiv','page','normal'); // author page
+
+     //remove_meta_box('revisionsdiv','post','normal'); // revisions post
+     //remove_meta_box('revisionsdiv','page','normal'); // revisions page
+
+     //remove_meta_box('postimagediv','post','normal'); // featured image post
+     remove_meta_box('postimagediv','page','normal'); // featured image page
+
+     //remove_meta_box('pageparentdiv','page','normal'); // page attributes
+
+     //remove_meta_box('tagsdiv-post-tag','post','normal'); // post tags
+     //remove_meta_box('categorydiv','post','normal'); // post categories
+     //remove_meta_box('postexcerpt','post','normal'); // post excerpt
+     remove_meta_box('trackbacksdiv','post','normal'); // track backs
+}
+add_action('admin_init', 'hide_meta_boxes');
+
+/* ========================================================================= */
+/*  Browser detection body_class() output */
+/* ========================================================================= */
+
+function alx_browser_body_class( $classes ) {
+    global $is_lynx, $is_gecko, $is_IE, $is_opera, $is_NS4, $is_safari, $is_chrome, $is_iphone;
+
+    if($is_lynx) $classes[] = 'lynx';
+    elseif($is_gecko) $classes[] = 'gecko';
+    elseif($is_opera) $classes[] = 'opera';
+    elseif($is_NS4) $classes[] = 'ns4';
+    elseif($is_safari) $classes[] = 'safari';
+    elseif($is_chrome) $classes[] = 'chrome';
+    elseif($is_IE) {
+        $browser = $_SERVER['HTTP_USER_AGENT'];
+        $browser = substr( "$browser", 25, 8);
+        if ($browser == "MSIE 7.0"  ) {
+            $classes[] = 'ie7';
+            $classes[] = 'ie';
+        } elseif ($browser == "MSIE 6.0" ) {
+            $classes[] = 'ie6';
+            $classes[] = 'ie';
+        } elseif ($browser == "MSIE 8.0" ) {
+            $classes[] = 'ie8';
+            $classes[] = 'ie';
+        } elseif ($browser == "MSIE 9.0" ) {
+            $classes[] = 'ie9';
+            $classes[] = 'ie';
+        } else {
+            $classes[] = 'ie';
+        }
+    }
+    else $classes[] = 'unknown';
+
+    if( $is_iphone ) $classes[] = 'iphone';
+
+    return $classes;
+}
+add_filter( 'body_class', 'alx_browser_body_class' );
+
 if ( ! class_exists( 'Timber' ) ) {
 	add_action( 'admin_notices', function() {
 			echo '<div class="error"><p>Timber not activated. Make sure you activate the plugin in <a href="' . esc_url( admin_url( 'plugins.php#timber' ) ) . '">' . esc_url( admin_url( 'plugins.php' ) ) . '</a></p></div>';
@@ -99,8 +180,11 @@ function myfoo( $text ) {
 
 //Styles
 function wpdocs_scripts() {
-    wp_enqueue_style( 'style', get_stylesheet_directory_uri().'/css/style.css', array(), '1.0.0', 'screen' );
-    //wp_enqueue_script( '', get_template_directory_uri() . '/js/example.js', array(), '1.0.0', true );
+	$tloc = get_stylesheet_directory_uri(); wp_deregister_script( 'jquery' );
+    wp_enqueue_style( 'style', $tloc.'/css/style.css', array(), '', 'screen' );
+    wp_enqueue_script( 'jquery', 'https://code.jquery.com/jquery-1.12.0.min.js', array(), '', false );
+	wp_enqueue_script( 'plugins', $tloc.'/js/plugins.js', array(), '', true );
+	wp_enqueue_script( 'main', $tloc.'/js/main.js', array(), '', true);
 }
 add_action( 'wp_enqueue_scripts', 'wpdocs_scripts' );
 //Javascripts
--- wp-content/themes/kevin/js/main.js
+++ wp-content/themes/kevin/js/main.js
@@ -1,16 +1,14 @@
-$(document).ready(function(){
-  $('a[href*=#]').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
-    && location.hostname == this.hostname) {
-      var $target = $(this.hash);
-      $target = $target.length && $target
-      || $('[name=' + this.hash.slice(1) +']');
-      if ($target.length) {
-        var targetOffset = $target.offset().top;
-        $('html,body')
-        .animate({scrollTop: targetOffset}, 1000);
-       return false;
-      }
-    }
-  });
+jQuery(function($){
+    $(window).scroll(function(){
+        if($(this).scrollTop() > 40){
+            $('header').addClass('fixed');
+        } else {
+            $('header').removeClass('fixed');
+        }
+        if($(this).scrollTop() > 45){
+            $('header').addClass('flipin');
+        } else {
+            $('header').removeClass('flipin');
+        }
+    });
 });
